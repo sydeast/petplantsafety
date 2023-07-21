b6d0be9c6bdcd42615563ab578e1c3ca234b8543
Syd Daniels <24235464+sydeast@users.noreply.github.com>
2023-07-21 15:05:16 -0500

    react setup

---
+++ plant-safety/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <div>
+      <p>This is just a placement</p>
+    </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
